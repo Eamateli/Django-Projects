fe302bb64a73e5fefb933e9b442796ec6d961518
Eamateli <e.a.mateli@gmail.com>
2025-05-15 17:26:44 +0300

    Created a view, template and added the url path

--- Beginner/real_estate/listings/views.py
+++ Beginner/real_estate/listings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Listing
 
 # Create your views here.
+def listings_list(request):
+    Listing.objects.all()
+    context = {
+        "listings": listings
+    }
+    return render(request, "listings.html", context)
